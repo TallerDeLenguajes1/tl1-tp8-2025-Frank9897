b7906120322e91e990e5628613f1f56524a510b2
Franco Vasquez <franco.vasquez9897@gmail.com>
2025-06-03 15:20:08 -0300

    Codificacion Ejercicio 1 punto 1 y 2

--- TodO/Program.cs
+++ TodO/Program.cs
@@ -1,2 +1,23 @@
 ﻿using esTarea;
 
+
+int numTareas = 3;
+Tarea[] tareas = new Tarea[numTareas];
+//Inicializo variables para el objeto tarea
+for (int i = 0; i < tareas.Length; i++)
+{
+    int tareasID = i + 1;
+    Console.WriteLine($"Ingrese el nombre de tarea {i + 1}");
+    string? descripcion = Console.ReadLine();
+    Console.WriteLine($"Ingrese la duracion de la tarea {i + 1}");
+    int duracion = int.Parse(Console.ReadLine());
+    tareas[i] = new Tarea(tareasID, descripcion, duracion);
+}
+//Creo las listas
+List<Tarea> listaTareasPendientes = new();
+List<Tarea> listaTareasRealizadas = new();
+
+for (int i = 0; i < tareas.Length; i++)
+{
+    listaTareasPendientes.Add(tareas[i]);
+}
