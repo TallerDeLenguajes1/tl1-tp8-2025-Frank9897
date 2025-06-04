495f765799eb86f97d6596576414090cf36c07e2
Vasquez Franco <franco.vasquez9897@gmail.com>
2025-06-04 00:49:21 -0300

    Realizando interfaz de usuario completando el ultimo apartado del punto 1

--- TodO/Program.cs
+++ TodO/Program.cs
@@ -1,87 +1,49 @@
 ﻿using esTarea;
 
 
-int numTareas = 3;
-Tarea[] tareas = new Tarea[numTareas];
-//Inicializo variables para el objeto tarea
-for (int i = 0; i < tareas.Length; i++)
-{
-    Random rand = new Random();
-    int tareasID = i + 1;
-    Console.WriteLine($"Ingrese La descripcion de tarea {i + 1}");
-    string? descripcion = Console.ReadLine();
-    int duracion = rand.Next(10, 100);
-    tareas[i] = new Tarea(tareasID, descripcion, duracion);
-}
+
 //Creo las listas
 List<Tarea> listaTareasPendientes = new();
 List<Tarea> listaTareasRealizadas = new();
-
-for (int i = 0; i < tareas.Length; i++)
+bool continuar = true;
+do
 {
-    listaTareasPendientes.Add(tareas[i]);
-}
-
-Console.WriteLine("-----------Tareas Pendientes-----------");
-foreach (var tarea in listaTareasPendientes)
-{
-    Console.WriteLine($"ID tarea: {tarea.TareaID}\nDescripcion: {tarea.Descripcion}\nDuracion {tarea.Duracion} min");
-}
-
-Console.WriteLine("Ingrese la ID de tarea que desea mover a tareas realizadas");
-int opc = int.Parse(Console.ReadLine());
-
-for (int i = listaTareasPendientes.Count - 1; i >= 0; i--)
-{
-
-    if (listaTareasPendientes[i].TareaID == opc)
+    Console.WriteLine("-----------MENU-----------\n1.Crear Tarea Pendiete\n2.Mover tareas pendiente a realizadas por ID\n3.Buscar tareas realizadas por palabra clave\n4.Mostrar listas\nIngrese opcion a elegir (0 para terimnar)");
+    int eleccion = int.Parse(Console.ReadLine());
+    switch (eleccion)
     {
-        listaTareasRealizadas.Add(listaTareasPendientes[i]);
-        Console.WriteLine($"Tarea ID: {opc} Movida con exito");
-        listaTareasPendientes.RemoveAt(i);
-        break;
+        case 1:
+            creartarea(listaTareasPendientes);
+            break;
+        case 2:
+            movertareaXid(listaTareasPendientes,listaTareasRealizadas);
+            break;
+        case 3:
+            buscarpordescripcion(listaTareasPendientes);
+            break;
+        case 4:
+            mostrarPendientes(listaTareasPendientes);
+            mostrarRealizadas(listaTareasRealizadas);   
+            break;
+        case 0:
+            continuar = false;
+            break;    
+        default:
+            Console.WriteLine("Opcion invalida...");
+            continue;
     }
-    
-}
+} while (continuar);
 
-Console.WriteLine("Ingresar una palabra clave para buscar la descripcion en tareas pendientes");
-string? pclave = Console.ReadLine();
 
-buscarpordescripcion(pclave,listaTareasPendientes);
 
-Console.WriteLine("-----------Tareas Pendientes-----------");
-if (listaTareasPendientes.Count == 0)
+static void buscarpordescripcion(List<Tarea> listaTareasPendientes)
 {
-    Console.WriteLine("Ninguna");
-}else
-{
-    foreach (Tarea lista1 in listaTareasPendientes)
-    {
-        Console.WriteLine($"Tarea ID: {lista1.TareaID}\nDescripcion: {lista1.Descripcion}\nDuracion: {lista1.Duracion} min");
-    }
-}
-
-Console.WriteLine("-----------Tareas Realizadas-----------");
-if (listaTareasRealizadas.Count == 0)
-{
-    Console.WriteLine("Ninguna");
-}else
-{
-    foreach (Tarea lista2 in listaTareasRealizadas)
-    {
-        Console.WriteLine($"Tarea ID: {lista2.TareaID}\nDescripcion: {lista2.Descripcion}\nDuracion: {lista2.Duracion} min");
-    }
-}
-
-
-
-
-static void buscarpordescripcion(string descripcion, List<Tarea> listaTareasPendientes)
-{
+    Console.WriteLine("Ingresar una palabra clave para buscar la descripcion en tareas pendientes");
+    string? pclave = Console.ReadLine();
     bool encontrada = false;
     foreach (Tarea tarea in listaTareasPendientes)
     {
-        if (tarea.Descripcion.Contains(descripcion,StringComparison.OrdinalIgnoreCase))
+        if (tarea.Descripcion.Contains(pclave, StringComparison.OrdinalIgnoreCase))
         {
             Console.WriteLine($"-----------Tarea encontrada-----------\nTarea ID: {tarea.TareaID}\nDescripcion: {tarea.Descripcion}\nDuracion: {tarea.Duracion} min\n");
             encontrada = true;
@@ -90,5 +52,72 @@ static void buscarpordescripcion(string descripcion, List<Tarea> listaTareasPend
     if (!encontrada)
     {
         Console.WriteLine("No se encontro ninguna descripcion.");
+    }
+}
+
+static void creartarea(List<Tarea> listaTareasPendientes )
+{
+    Random rand = new Random();
+    int control = listaTareasPendientes.Count;
+    int tareasID = listaTareasPendientes.Count + 1;
+    Console.WriteLine("Ingrese La descripcion de tarea");
+    string? descripcion = Console.ReadLine();
+    int duracion = rand.Next(10, 100);
+    listaTareasPendientes.Add(new Tarea(tareasID, descripcion, duracion));
+    if (listaTareasPendientes.Count > control)
+    {
+        Console.WriteLine("Se aniadio la tarea correctamente.");
+    }else
+    {
+        Console.WriteLine("No se pudo cargar la tarea");
+    }
+}
+
+static void mostrarPendientes(List<Tarea> listaTareasPendientes)
+{
+    Console.WriteLine("-----------Tareas Pendientes-----------");
+    if (listaTareasPendientes.Count == 0)
+    {
+        Console.WriteLine("Ninguna");
+    }else
+    {
+        foreach (Tarea lista1 in listaTareasPendientes)
+        {
+            Console.WriteLine($"Tarea ID: {lista1.TareaID}\nDescripcion: {lista1.Descripcion}\nDuracion: {lista1.Duracion} min");
+        }
+    }
+}
+
+static void mostrarRealizadas(List<Tarea> listaTareasRealizadas)
+{
+    Console.WriteLine("-----------Tareas Realizadas-----------");
+    if (listaTareasRealizadas.Count == 0)
+    {
+        Console.WriteLine("Ninguna");
+    }else
+    {
+        foreach (Tarea lista2 in listaTareasRealizadas)
+        {
+            Console.WriteLine($"Tarea ID: {lista2.TareaID}\nDescripcion: {lista2.Descripcion}\nDuracion: {lista2.Duracion} min");
+        }
+    }
+}
+
+static void movertareaXid(List<Tarea> listaTareasPendientes,List<Tarea> listaTareasRealizadas)
+{
+    Console.WriteLine("Ingrese la ID de tarea que desea mover a tareas realizadas");
+    int opc = int.Parse(Console.ReadLine());
+
+    for (int i = listaTareasPendientes.Count - 1; i >= 0; i--)
+    {
+
+        if (listaTareasPendientes[i].TareaID == opc)
+        {
+            listaTareasRealizadas.Add(listaTareasPendientes[i]);
+            Console.WriteLine($"Tarea ID: {opc} Movida con exito");
+            listaTareasPendientes.RemoveAt(i);
+            break;
+        }
+        
     }
 }
